8eb9c6a191a1a3e70d8048b754cf3264f90a28c5
akash2061 <lucifermorningstar2061@gmail.com>
2024-07-10 21:25:35 +0530

    Variables

--- src/App.java
+++ src/App.java
@@ -1,6 +1,28 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        // ! Starting
         System.out.println("Hello, World!");
-        System.out.println();
+
+        // ! Variables
+        System.out.println("\nVariables:");
+        byte b = 127;
+        short s = 2000;
+        int i = 100;
+        double d = 10.5;
+        float f = 3.14159f;
+        long l = 1000516584351681310L;
+        char c = '@';
+        boolean bool = true;
+        String str = "Morningstar!";
+
+        System.out.println("char:\t" + c);
+        System.out.println("boolean:\t" + bool);
+        System.out.println("String:\t" + "Hello, " + str);
+        System.out.println("long:\t" + l);
+        System.out.println("int:\t" + i);
+        System.out.println("short:\t" + s);
+        System.out.println("float:\t" + f);
+        System.out.println("double:\t" + d);
+        System.out.println("byte:\t" + b);
     }
 }

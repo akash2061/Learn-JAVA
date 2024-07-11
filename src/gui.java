72d8c27a8582f5952638c044610d6f66982597c7
akash2061 <lucifermorningstar2061@gmail.com>
2024-07-11 14:02:46 +0530

    GUI update

--- src/gui.java
+++ src/gui.java
@@ -4,5 +4,7 @@ public class gui {
     public static void main(String[] args) {
         String name = JOptionPane.showInputDialog("Enter Your Name:");
         JOptionPane.showMessageDialog(null, "Hello " + name);
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter Your Age:"));
+        JOptionPane.showMessageDialog(null, "You are " + age + " yera old.");
     }
 }

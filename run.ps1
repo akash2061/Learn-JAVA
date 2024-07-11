4572c50574f1c8c217ac1f3a7369fb5235361068
akash2061 <lucifermorningstar2061@gmail.com>
2024-07-11 13:45:01 +0530

    Help-Updated

--- run.ps1
+++ run.ps1
@@ -1,7 +1,20 @@
 # Define the commands
-$compile = "javac -d bin .\src\App.java"
-$run = "java -cp bin App"
+$compile = "javac -d bin .\src\*.java"
 
+# Prompt the user to choose which Java application to run
+$choice = Read-Host "Which Java application do you want to run? Enter 'App' or 'gui' or enter file name"
+
+# Set the run command based on user input
+# if ($choice -eq "App") {
+#     $run = "java -cp bin App"
+# } elseif ($choice -eq "gui") {
+#     $run = "java -cp bin gui"
+# } else {
+#     Write-Host "Invalid choice. Exiting script."
+#     exit
+# }
+$run = "java -cp bin $choice"
+""
 # Execute the commands
 Invoke-Expression $compile
 Invoke-Expression $run

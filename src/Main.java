dd5d441ca1517a746726ebc564605ea174af907c
akash2061 <lucifermorningstar2061@gmail.com>
2024-07-15 18:26:40 +0530

    Switch Case

--- src/Main.java
+++ src/Main.java
@@ -111,6 +111,35 @@ public class Main {
             System.out.println("You are a child. " + "Age : " + age);
         }
 
+        // ! Switch Case
+        String day = "Friday";
+        switch (day) {
+            case "Sunday":
+                System.out.println("Today is Sunday");
+                break;
+            case "Monday":
+                System.out.println("Today is Monday");
+                break;
+            case "Tuesday":
+                System.out.println("Today is Tuesday");
+                break;
+            case "Wednesday":
+                System.out.println("Today is Wednesday");
+                break;
+            case "Thursday":
+                System.out.println("Today is Thursday");
+                break;
+            case "Friday":
+                System.out.println("Today is Friday");
+                break;
+            case "Saturday":
+                System.out.println("Today is Saturday");
+                break;
+            default:
+                System.out.println("Invalid day options");
+                break;
+        }
+
         scanner.close();
     }
 }

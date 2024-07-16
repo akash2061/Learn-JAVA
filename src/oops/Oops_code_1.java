ffa47b7a31fb403d271dd37657abca21804d6383
akash2061 <lucifermorningstar2061@gmail.com>
2024-07-16 20:13:33 +0530

    Constructors

--- src/oops/Oops_code_1.java
+++ src/oops/Oops_code_1.java
@@ -3,6 +3,7 @@ package oops;
 public class Oops_code_1 {
     public static void main(String[] args) {
         car();
+        human();
     }
 
     static void car() {
@@ -13,5 +14,21 @@ public class Oops_code_1 {
         System.out.println("\nCalling Car-Functions:\n");
         c.drive();
         c.brake();
+    }
+
+    static void human() {
+        Human h = new Human("Morningstar", 20, 65);
+        System.out.println("\nHuman Description:\n");
+        System.out.printf("Name: %18s\n", h.name);
+        System.out.printf("Age: %10d\n", h.age);
+        System.out.printf("Weight: %10.2f\n", h.weight);
+        h.eat();
+        System.out.println();
+        Human h1 = new Human("Vincent", 27, 78);
+        System.out.printf("Name: %14s\n", h1.name);
+        System.out.printf("Age: %10d\n", h1.age);
+        System.out.printf("Weight: %10.2f\n", h1.weight);
+        h1.drink();
+        System.out.println();
     }
 }

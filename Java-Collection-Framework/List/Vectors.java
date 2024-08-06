bab045065c894c0ccc97c4c01ab8b39c09660816
akash2061 <lucifermorningstar2061@gmail.com>
2024-08-06 22:22:18 +0530

    Vectors

---
+++ Java-Collection-Framework/List/Vectors.java
@@ -0,0 +1,44 @@
+package List;
+
+import java.util.Vector;
+import java.util.Scanner;
+
+public class Vectors {
+    public static void main(String[] args) {
+        Vector<Integer> v = new Vector<>();
+        v.add(null);
+        v.add(1);
+        v.add(2);
+        v.add(3);
+        v.add(4);
+        v.add(0, 5);
+
+        System.out.println("\nElements in Vector: ");
+        for (var x : v) {
+            System.out.println(x);
+        }
+        System.out.println("\n" + v);
+        System.out.println("\nFirst Element: " + v.getFirst());
+        System.out.println("Last Element:  " + v.getLast());
+        v.remove(1);
+        v.removeFirst();
+        v.removeLast();
+        System.out.println("\nRemoved First && Last && 1st index element: " + v);
+        v.clear();
+
+        // ! Take input.
+        System.out.println("\nEnter 5 Element to Vector:");
+        Scanner s = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            try {
+                int x = s.nextInt();
+                v.add(x);
+            } catch (Exception e) {
+                System.err.println("Invalid input: " + e.getMessage());
+                s.nextLine();
+            }
+        }
+        s.close();
+        System.out.println("\nLinked-List Contains: " + v);
+    }
+}

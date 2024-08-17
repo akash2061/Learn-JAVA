cef700e5fc4b4dc5eb37323807e7e71c0e5faa14
akash2061 <lucifermorningstar2061@gmail.com>
2024-08-17 23:03:11 +0530

    Hash_Map

---
+++ Java-Collection-Framework/Map/Hash_Map.java
@@ -0,0 +1,10 @@
+package Map;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Hash_Map {
+    public static void main(String[] args) {
+        Map<String, Integer> m = new HashMap<>();
+    }
+}
--- Java-Collection-Framework/Map/Map_1.java
+++
@@ -1,7 +0,0 @@
-package Map;
-
-public class Map_1 {
-    public static void main(String[] args) {
-        
-    }
-}

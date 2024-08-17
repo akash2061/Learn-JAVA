a0167aa84df31cd0f42cbbb268db283aef278bc3
akash2061 <lucifermorningstar2061@gmail.com>
2024-08-17 23:12:53 +0530

    HashMap storing HashCode

--- Java-Collection-Framework/Map/Hash_Map.java
+++ Java-Collection-Framework/Map/Hash_Map.java
@@ -6,5 +6,16 @@ import java.util.HashMap;
 public class Hash_Map {
     public static void main(String[] args) {
         Map<String, Integer> m = new HashMap<>();
+        m.put("Apple", 3);
+        m.put("Banana", 2);
+        m.put("Orange", 5);
+
+        String orange = "Orange";
+        String foo = "Foo";
+
+        m.put(orange, orange.hashCode()); //? Replace value (5) with hashCode.
+        m.put(foo, foo.hashCode());
+
+        System.out.println("\nMap: " + m);
     }
 }

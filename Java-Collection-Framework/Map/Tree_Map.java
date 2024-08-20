4f28add50acf11ede45b8a33dd87e9c0091bc252
akash2061 <lucifermorningstar2061@gmail.com>
2024-08-20 19:39:07 +0530

    Sorted Tree-Map

--- Java-Collection-Framework/Map/Tree_Map.java
+++ Java-Collection-Framework/Map/Tree_Map.java
@@ -13,13 +13,26 @@ public class Tree_Map {
         m.put("Pumpkin-Seeds", 1);
         m.put("Chocolate-Cake", 4);
         m.put("Avocado", 6);
+        System.out.println("\nTree-Map Contains: ");
+        for (Map.Entry<String, Integer> i : m.entrySet()) {
+            System.out.println(i.getKey() + "    :  " + i.getValue());
+        }
 
-        System.out.println("\nTree-Map Contains: " + m);
-
+        // ! Tree-Map in Reverse Order.
         Map<String, Integer> rev = new TreeMap<>(Comparator.reverseOrder());
         for (Map.Entry<String, Integer> entry : m.entrySet()) {
             rev.put(entry.getKey(), entry.getValue());
         }
         System.out.println("\nTree-Map Contains (reverse): " + rev);
+
+        // ! Sorting based on numbers:
+        Map<Integer, String> m1 = new TreeMap<>();
+        for (Map.Entry<String, Integer> v : m.entrySet()) {
+            m1.put(v.getValue(), v.getKey());
+        }
+        System.out.println("\nTree-Map Sorted Key: ");
+        for (Map.Entry<Integer, String> i : m1.entrySet()) {
+            System.out.println(i.getKey() + "\t:\t" + i.getValue());
+        }
     }
 }

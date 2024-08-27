9e8983c661de4ea8022b6e94e80aaa5bd80b356a
akash2061 <lucifermorningstar2061@gmail.com>
2024-08-27 13:22:43 +0530

    File Write & exit

--- src/Files.java
+++ src/Files.java
@@ -1,15 +1,16 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Files {
     public static void main(String[] args) throws IOException {
         File file = new File("test.txt");
         if (file.exists()) {
-            System.out.println("File exists: " + file.getName() + "\nPath: " + file.getAbsolutePath());
+            System.out.println("\nFile exists: " + file.getName() + "\nPath: " + file.getAbsolutePath());
             System.out.println("Is it a File? " + file.isFile());
         } else {
-            System.out.println("File does not exist.");
+            System.out.println("\nFile does not exist.");
         }
         w(file);
     }
@@ -18,8 +19,19 @@ public class Files {
         if (!f.exists()) {
             System.out.println("File does not exist.");
         }
-        try (FileWriter W = new FileWriter(f)) {
-            W.write("Hello Java.");
+        try (FileWriter W = new FileWriter(f, true); // 'true' for appending to file
+                Scanner s = new Scanner(System.in)) {
+
+            System.out.println("\nEnter a String to add to File: " + f.getAbsolutePath());
+            System.out.println("Type 'exit' to stop writing.");
+
+            while (true) {
+                String input = s.nextLine(); // Read the entire line
+                if (input.equals("exit")) {
+                    break;
+                }
+                W.write(input + System.lineSeparator()); // Add newline after each input
+            }
         }
     }
 }
